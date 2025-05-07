d7537f81ef2f11302d0517e410c75d0d4b72eba4
Anant Lalwani <anantlalwani2712@gmail.com>
2025-05-07 16:54:39 +0530

    Commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>About SkillHub</h2>
-      <p>This is about section.</p>
-    </div>
+    <section className="about-section">
+      <div className="about-us">
+        <h2>About Us</h2>
+        <p>
+        SkillHub's mission is to permeate through every student/professional's outlook towards jobs and change their attitude and perspective from "How Can I Do It?" to "Of Course I Can Do It". We aim to do this by providing exceptional up skilling courses at affordable rates, while being tech-forward so anyone, anywhere can access and improve their ability to be successful in life.
+        </p>
+      </div>
+
+      <div className="mission-section">
+        <h3>Our Mission</h3>
+        <p>
+        SkillHub is the result of a continual effort to exponentially increase the employability of every Indian, irrespective of their socioeconomic status. With accessibility and affordability being the support structure of high-quality, industry-relevant courses, SkillHub aims to empower professionals and students alike to either jumpstart their careers or leverage existing skills with new, future-driven upgrades that will help them realise their full potential.
+        </p>
+      </div>
+    </section>
   );
 };
 
